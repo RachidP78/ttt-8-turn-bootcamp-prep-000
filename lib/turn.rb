107e6a8255fcbc00599d3b11ee1308970af2d25d
Rachid Abu-Hassan <rachid@platform78.com>
2020-11-24 20:02:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,6 +3,7 @@ def turn(board)
   index = input_to_index(gets.strip)
   if valid_move?(board, index) == true
     move(board, index, value = "X")
+    display_board(board)
   else turn(board)
   end
 end
