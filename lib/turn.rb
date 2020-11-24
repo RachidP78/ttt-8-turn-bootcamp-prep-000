9638a76f5a3ecf7ee882538f58ed1a49dd284298
Rachid Abu-Hassan <rachid@platform78.com>
2020-11-24 19:58:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,7 +30,7 @@ def position_taken?(board, index)
 end
 
 def input_to_index(user_input)
-  index = user_input.to_i - 1
+  return index = user_input.to_i - 1
 end
 
 def move(board, index, value = "X")
